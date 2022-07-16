7f28603cfc9de4bdbba4eb408a37424fc5c0a296
Ravindra Vala <55114723+ravindravala@users.noreply.github.com>
2022-07-16 20:28:23 +0530

    :bug: close webview after login

--- lib/Controller/instagram_login.dart
+++ lib/Controller/instagram_login.dart
@@ -1,28 +1,36 @@
- import 'dart:io';
+import 'dart:io';
 
- import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
- class InstaLogin extends StatefulWidget {
-   @override
-   InstaLoginState createState() => InstaLoginState();
- }
+class InstaLogin extends StatefulWidget {
+  @override
+  InstaLoginState createState() => InstaLoginState();
+}
 
- class InstaLoginState extends State<InstaLogin> {
-   @override
-   void initState() {
-     super.initState();
-     // Enable virtual display.
-     if (Platform.isAndroid) WebView.platform = AndroidWebView();
-   }
+class InstaLoginState extends State<InstaLogin> {
+  @override
+  void initState() {
+    super.initState();
+    // Enable virtual display.
+    if (Platform.isAndroid) WebView.platform = AndroidWebView();
+  }
 
-   @override
-   Widget build(BuildContext context) {
-     return SafeArea(
-       child: WebView(
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: WebView(
         javascriptMode: JavascriptMode.unrestricted,
-         initialUrl: 'https://www.instagram.com/accounts/login/',
-       ),
-     );
-   }
- }
+        initialUrl: 'https://www.instagram.com/accounts/login/',
+        navigationDelegate: (navigation) {
+          if (!navigation.url.contains("https://www.instagram.com/accounts/login/")) {
+            Navigator.pop(context);
+            return NavigationDecision.prevent;
+          } else {
+            return NavigationDecision.navigate;
+          }
+        },
+      ),
+    );
+  }
+}
